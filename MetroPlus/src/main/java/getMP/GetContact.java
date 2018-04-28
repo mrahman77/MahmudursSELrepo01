8dc923e78c2b295786bd941288b7d3bdc5f1389d
mrahman77 <mikatmahmud@gmail.com>
2018-04-28 13:17:40 -0400

    added Contact Us

---
+++ MetroPlus/src/main/java/getMP/GetContact.java
@@ -0,0 +1,8 @@
+package getMP;
+
+/**
+ * Created by mahmudurrahman
+ * On 4/28/18.
+ */
+public class GetContact {
+}
---
+++ MetroPlus/src/test/java/openWS/test02.java
@@ -0,0 +1,8 @@
+package openWS;
+
+/**
+ * Created by mahmudurrahman
+ * On 4/28/18.
+ */
+public class test02 {
+}
